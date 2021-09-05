87987e915975b732a438ce7556e8796c2e86da7d
samar3366 <sumanthreddypathi3366@gmail.com>
2021-09-05 15:34:26 +0530

    header minor fix

--- app/src/layout/Header.tsx
+++ app/src/layout/Header.tsx
@@ -27,6 +27,7 @@ const Header: React.FC<any> = (props) => {
                     <a href={routes.aboutUs}>About Us</a>
                     <a href={routes.itStaffing}>IT Staffing</a>
                     <a href={routes.projects}>Projects</a>
+                    <a href={routes.blog}>Blog</a>
                   </div>
                 </div>
               </li>
@@ -47,12 +48,6 @@ const Header: React.FC<any> = (props) => {
                 </a>
               </li>
               <li className="nav-item">
-                {" "}
-                <a href={routes.blog} className="menu-item">
-                  Blog
-                </a>
-              </li>
-              <li className="nav-item">
                 <a href={routes.contactUs} className="menu-item">
                   Contact Us
                 </a>
@@ -70,7 +65,7 @@ const Header: React.FC<any> = (props) => {
             </ul>
           </div>
         </nav>
-        <div className="navbar navbar-expand-md navbar-dark mobile-device-show">
+        <div className="navbar fixed-top navbar-light mobile-device-show">
           <div className="container-fluid">
             <a className="navbar-brand" href={routes.home}>
               <img src={Logo} alt="" className="header-logo" />
@@ -89,15 +84,18 @@ const Header: React.FC<any> = (props) => {
             <div className="collapse navbar-collapse" id="navbarToggler">
               <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                 <li className="nav-item">
-                  <div className="menu-item-active">Home</div>
+                  <a href={routes.home} className="menu-item-active">
+                    Home
+                  </a>
                 </li>
                 <li className="nav-item">
                   <div className="menu-item">
                     <div className="menu-label">Company</div>
                     <div className="menu-list">
-                      <a href={routes.home}>About Us</a>
-                      <a href={routes.home}>IT Staffing</a>
-                      <a href={routes.home}>Projects</a>
+                      <a href={routes.aboutUs}>About Us</a>
+                      <a href={routes.itStaffing}>IT Staffing</a>
+                      <a href={routes.projects}>Projects</a>
+                      <a href={routes.blog}>Blog</a>
                     </div>
                   </div>
                 </li>
@@ -105,28 +103,32 @@ const Header: React.FC<any> = (props) => {
                   <div className="menu-item">
                     <div className="menu-label">Courses</div>
                     <div className="menu-list">
-                      <a href={routes.home}>Corporate Training</a>
-                      <a href={routes.home}>Online Training</a>
-                      <a href={routes.home}>Certifications</a>
+                      <a href={routes.corporateTraining}>Corporate Training</a>
+                      <a href={routes.onlineTraining}>Online Training</a>
+                      <a href={routes.certifications}>Certifications</a>
                     </div>
                   </div>
                 </li>
-                <li>
+                <li className="nav-item">
                   {" "}
-                  <div className="menu-item">Become Trainer</div>
+                  <a href={routes.becomeTrainer} className="menu-item">
+                    Become Trainer
+                  </a>
                 </li>
-                <li>
-                  {" "}
-                  <div className="menu-item">Blog</div>
+                <li className="nav-item">
+                  <a href={routes.contactUs} className="menu-item">
+                    Contact Us
+                  </a>
                 </li>
-                <li>
-                  <div className="menu-item">Contact Us</div>
+                <li className="nav-item">
+                  <a href={routes.home} className="sign-up">
+                    Sign Up
+                  </a>
                 </li>
-                <li>
-                  <div className="sign-up">Sign Up</div>
-                </li>
-                <li>
-                  <div className="login">Login</div>
+                <li className="nav-item">
+                  <a href={routes.home} className="login">
+                    Login
+                  </a>
                 </li>
               </ul>
             </div>
