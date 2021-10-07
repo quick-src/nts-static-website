708b24f31a5c4839fccb47a104c6409745d0c488
samar3366 <sumanthreddypathi3366@gmail.com>
2021-10-07 14:11:10 +0530

    side icon sticky bar

--- app/src/layout/Header.tsx
+++ app/src/layout/Header.tsx
@@ -8,6 +8,23 @@ import "./Header.css";
 const Header: React.FC<any> = (props) => {
   return (
     <>
+      <div className="icon-bar">
+        <a href="#" className="facebook">
+          <i className="fa fa-facebook"></i>
+        </a>
+        <a href="#" className="twitter">
+          <i className="fa fa-twitter"></i>
+        </a>
+        <a href="#" className="google">
+          <i className="fa fa-google"></i>
+        </a>
+        <a href="#" className="linkedin">
+          <i className="fa fa-linkedin"></i>
+        </a>
+        <a href="#" className="youtube">
+          <i className="fa fa-youtube"></i>
+        </a>
+      </div>
       <header>
         <nav className="navbar fixed-top navbar-light desktop-device-show">
           <div className="sub-header">
