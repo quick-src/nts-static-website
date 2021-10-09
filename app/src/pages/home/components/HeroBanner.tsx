7903a7d3a254501f0e54761731c004f0ed8e41d8
samar3366 <sumanthreddypathi3366@gmail.com>
2021-10-09 10:54:45 +0530

    hero banner

--- app/src/pages/home/components/HeroBanner.tsx
+++ app/src/pages/home/components/HeroBanner.tsx
@@ -4,35 +4,45 @@ import Slider from "react-slick";
 import "./HeroBanner.css";
 
 const HeroBanner: React.FC<any> = (props) => {
-  const settings = {
+  const settings: any = {
+    lazyLoad: true,
     dots: true,
+    infinite: true,
+    speed: 500,
+    autoplay: true,
+    autoplaySpeed: 2500,
   };
+
   return (
     <div className="hero-banner">
-      <div className="container">
-        <Slider {...settings}>
-          <div className="slide-1">
+      <Slider {...settings}>
+        <div className="slide-1">
+          <div className="container">
             <h1>
               Finding the Right IT Solutions for Your Business
               <br />
               Simplify your day-to-day Business requirements
             </h1>
           </div>
-          <div className="slide-1">
+        </div>
+        <div className="slide-2">
+          <div className="container">
             <h1>
               Keep your skills updated in NTS, Reach real career growth <br />
               Get Certified, Get Ahead with Our Programs
             </h1>
           </div>
-          <div className="slide-1">
+        </div>
+        <div className="slide-3">
+          <div className="container">
             <h1>
               Flexible IT staffing to enhance your team or project
               <br />
               Start improving your Employee Experience
             </h1>
           </div>
-        </Slider>
-      </div>
+        </div>
+      </Slider>
     </div>
   );
 };
